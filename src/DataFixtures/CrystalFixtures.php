ad594d08c2c4d182dbfbadf9c0303b78171cd177
guedel <guedel87@live.fr>
2021-12-27 11:48:30 +0100

    Add Crystal fixtures

---
+++ src/DataFixtures/CrystalFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Crystal;
+use App\Entity\Element;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CrystalFixtures extends CsvFileFixtures
+{
+    public function load(ObjectManager $manager): void
+    {
+        foreach ($this->doLoad('Crystals.csv') as $row)
+        {
+            $nature = $manager->getRepository(Element::class)->findOneBy(['nom' => $row[1]]);
+            $entity = $manager->getRepository(Crystal::class)->findOneBy(['nom' => $row[0]]);
+            if (! $nature instanceof Elemeent) {
+                continue;
+            }
+            if (! $entity instanceof Crystal) {
+                $entity = (new Crystal())
+                    ->setNom($row[0])
+                ;
+                $manager->persist($entity);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ElementFixtures::class
+        ];
+    }
+}
